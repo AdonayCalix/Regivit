7f891cd940fce7be3b814a37659b07910d9f81c0
AdonayCalix <adonaymorazan@gmail.com>
2018-09-14 04:36:53 -0600

    add

--- resources/views/candidate/solapa_dos.blade.php
+++ resources/views/candidate/solapa_dos.blade.php
@@ -1,51 +1,30 @@
-
-<!DOCTYPE html>
-<html lang="es">
-
-@include('layouts.head')
-
-<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
 <script src="{{asset('js/dropzone.js')}}"></script>
-
-@include('layouts.header')
-
-<div class="app-body">
-    <div class="sidebar">
-        @include('layouts.sidebar_aspirante')
+<br><br>
+<div class="animated fadeIn card">
+    <div class="card-header">
+        <i class="far fa-folder-open"></i> Solapa N° 2
     </div>
-    <div class="main">
-        <div class="container-fluid">
-            <div id="contenido">
-                <br><br>
-                <div class="animated fadeIn card">
-                    <div class="card-header">
-                        <i class="far fa-folder-open"></i> Solapa N° 2
-                    </div>
-                    <div class="card-body">
+    <div class="card-body">
 
-                        <div class="list-group hover">
-                            @foreach($document_list as $list)
-                                @if($list['visibility'] != 2)
-                                    <li data-toggle="modal"
-                                        data-target="#modal_formulario"
-                                        class="list-group-item list-group-item-action click"
-                                        data-flag="{{$list['id']}}">{{$list['name']}}
-                                        @if ($list['status'] === 'Upload')
-                                            <div class="col-sm-2 justify-content-center float-right">
-                                                <a class="item fas fa-check-circle font-sm text-success col-sm-1 float-right"></a>
-                                            </div>
-                                        @else
-                                            <div class="col-sm-2 justify-content-center float-right">
-                                                <a class="item fas fa-upload font-sm text-info col-sm-1 float-right"></a>
-                                            </div>
-                                        @endif
-                                    </li>
-                                @endif
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="list-group hover">
+            @foreach($document_list as $list)
+                @if($list['visibility'] != 2)
+                    <li data-toggle="modal"
+                        data-target="#modal_formulario"
+                        class="list-group-item list-group-item-action click"
+                        data-flag="{{$list['id']}}">{{$list['name']}}
+                        @if ($list['status'] === 'Upload')
+                            <div class="col-sm-2 justify-content-center float-right">
+                                <a class="item fas fa-check-circle font-sm text-success col-sm-1 float-right"></a>
+                            </div>
+                        @else
+                            <div class="col-sm-2 justify-content-center float-right">
+                                <a class="item fas fa-upload font-sm text-info col-sm-1 float-right"></a>
+                            </div>
+                        @endif
+                    </li>
+                @endif
+            @endforeach
         </div>
     </div>
 </div>
@@ -80,11 +59,6 @@
         </div>
     </div>
 </div>
-
-
-@include('layouts.footer')
-
-@include('layouts.scripts')
 
 <script>
     $(document).ready(function () {
@@ -179,5 +153,3 @@
     timer = setInterval(showRemaining, 1000);
 </script>
 
-</body>
-</html>
