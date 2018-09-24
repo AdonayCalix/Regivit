17b82e925dc7b0ef688bb1f403bcf4c56995a31e
AdonayCalix <adonaymorazan@gmail.com>
2018-09-24 14:41:43 -0600

    add

--- resources/views/candidate/vista_datos_personales.blade.php
+++ resources/views/candidate/vista_datos_personales.blade.php
@@ -518,7 +518,7 @@
     function getScreen() {
         var data_uri;
         html2canvas(document.querySelector("#capture"), {
-            width: 1070,
+            width: 1080,
             height: 3647
         }).then(canvas => {
             console.log(canvas.toDataURL());
