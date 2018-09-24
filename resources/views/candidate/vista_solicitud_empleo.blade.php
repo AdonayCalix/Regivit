9768929f13025e839c94456602163230901ee227
AdonayCalix <adonaymorazan@gmail.com>
2018-09-24 05:06:46 -0600

    add

--- resources/views/candidate/vista_solicitud_empleo.blade.php
+++ resources/views/candidate/vista_solicitud_empleo.blade.php
@@ -592,7 +592,7 @@
     function getScreen() {
         var data_uri;
         html2canvas(document.querySelector("#capture"), {
-            width: 1000,
+            width: 703,
             height: 3940.2
         }).then(canvas => {
             console.log(canvas.toDataURL());
