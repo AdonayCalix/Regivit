4dde454108c6208f65858db5872ed9caa71ad750
AdonayCalix <adonaymorazan@gmail.com>
2018-09-27 07:53:34 -0600

    add

--- resources/views/candidate/vista_datos_personales.blade.php
+++ resources/views/candidate/vista_datos_personales.blade.php
@@ -367,7 +367,7 @@
                                 </label><br>
                                 <div class="form-control align-content-center" id="contenedor_firma"
                                      style="height: 100px">
-                                    <img src="{{asset('uploades/' . $item->signature_path)}}" alt="" id="img-firma" width="100%" height="100%">
+                                    <img src="{{asset('uploades/' . $personal_data->signature_path)}}" alt="" id="img-firma" width="100%" height="100%">
                                 </div>
                                 <input type="hidden" name="signature_path" id="firma" value="soylafirma">
                             </div>
