f2a78756222682c4a7f34126346aa1a55446f97a
AdonayCalix <adonaymorazan@gmail.com>
2018-09-28 21:20:52 -0600

    add

--- app/Http/Controllers/ScreenFormController.php
+++ app/Http/Controllers/ScreenFormController.php
@@ -122,10 +122,10 @@ class ScreenFormController extends Controller
                 $sheet->setCellValue('T8', auth()->user()->first_surname);
                 $sheet->setCellValue('AA8', auth()->user()->second_surname);
                 switch ($value['civil_status']) {
-                    case 'Soltero (a)':
+                    case 'Soltero(a)':
                         $sheet->setCellValue('L11', 'X');
                         break;
-                    case 'Casado (a)':
+                    case 'Casado(a)':
                         $sheet->setCellValue('R11', 'X');
                         break;
                     case 'Union libre':
